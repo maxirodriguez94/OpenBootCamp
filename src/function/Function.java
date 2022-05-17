0763b35d5d4f99b7ca5a6f687aa1e96e94d0c03e
Maximiliano <maxi.rodriguez94@gmail.com>
2022-05-17 11:32:39 -0300

    exersice 2

---
+++ src/function/Function.java
@@ -0,0 +1,20 @@
+package function;
+
+import java.util.Scanner;
+
+public class Function {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un precio");
+        double price = scanner.nextDouble();
+        double priceIva = getPrice(price);
+        System.out.println("$"+priceIva);
+    }
+
+    static double getPrice(double price) {
+        // Porcentaje de iva en argentina
+        double iva = (price * 21) / 100;
+        return price + iva;
+    }
+}
